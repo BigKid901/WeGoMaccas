9ecca19d32b2baa12c7e092c1b030c72060783fd
Camron Braden <CamronRBraden@gmail.com>
2022-01-13 18:51:04 -0600

    made game work (for real this time)

--- actualGame/theGameA.js
+++ actualGame/theGameA.js
@@ -4,49 +4,90 @@ let keyboard = {}; //This is the link for keycodes to change controls http://gcc
     keyboard.UP = 87;     //W
     keyboard.DOWN = 83;   //S
     keyboard.LEFT = 65;   //A
-    keyboard.RIGHT = 68;  //D       
+    keyboard.RIGHT = 68;  //D     
+    
+    keyboard.UP_AGAIN = 73;     //I
+    keyboard.DOWN_AGAIN = 75;   //J
+    keyboard.LEFT_AGAIN = 74;   //K
+    keyboard.RIGHT_AGAIN = 76;  //L 
 
-let ourPlayer = { /* Player's start position and id, PlayerLocation */
+let ourPlayer1 = { /* Player's start position and id, PlayerLocation */
     x: 100,
     y: 100,
     speedMultiplier: 35,
     playerId: document.getElementById("thePlayer1")
 };
 
-let movePlayer = function(theX, theY) { /* Character Movement Updating */
-   ourPlayer.x += (theX||0) * ourPlayer.speedMultiplier;
-   ourPlayer.y += (theY||0) * ourPlayer.speedMultiplier;
-   ourPlayer.playerId.style.left = ourPlayer.x + 'px';
-   ourPlayer.playerId.style.top = ourPlayer.y + 'px';
+let movePlayer1 = function(theX, theY) { /* Character Movement Updating */
+   ourPlayer1.x += (theX||0) * ourPlayer1.speedMultiplier;
+   ourPlayer1.y += (theY||0) * ourPlayer1.speedMultiplier;
+   ourPlayer1.playerId.style.left = ourPlayer1.x + 'px';
+   ourPlayer1.playerId.style.top = ourPlayer1.y + 'px';
+};
+
+
+let ourPlayer2 = { /* Player's start position and id, PlayerLocation */
+    x: 100,
+    y: 100,
+    speedMultiplier: 35,
+    playerId: document.getElementById("thePlayer2")
+};
+
+let movePlayer2 = function(theX, theY) { /* Character Movement Updating */
+   ourPlayer2.x += (theX||0) * ourPlayer2.speedMultiplier;
+   ourPlayer2.y += (theY||0) * ourPlayer2.speedMultiplier;
+   ourPlayer2.playerId.style.left = ourPlayer2.x + 'px';
+   ourPlayer2.playerId.style.top = ourPlayer2.y + 'px';
 };
     
-let playerMotion = function() { /* Player Controls */
+let playerMotion1 = function() { /* Player Controls */
     if(keyboard[keyboard.LEFT]) {
-        movePlayer(-1,0);
+        movePlayer1(-1,0);
     }
     if(keyboard[keyboard.RIGHT]) {
-        movePlayer(1,0);
+        movePlayer1(1,0);
     }
     if(keyboard[keyboard.UP]) {
-        movePlayer(0,-1);
+        movePlayer1(0,-1);
     }
     if(keyboard[keyboard.DOWN]) {
-        movePlayer(0,1);
+        movePlayer1(0,1);
+    }
+}
+
+let playerMotion2 = function() { /* Player Controls */
+    if(keyboard[keyboard.LEFT_AGAIN]) {
+        movePlayer2(-1,0);
+    }
+    if(keyboard[keyboard.RIGHT_AGAIN]) {
+        movePlayer2(1,0);
+    }
+    if(keyboard[keyboard.UP_AGAIN]) {
+        movePlayer2(0,-1);
+    }
+    if(keyboard[keyboard.DOWN_AGAIN]) {
+        movePlayer2(0,1);
     }
 }
 
 function theControls(e) { 
    if(e.keyCode == 49 || e.keyCode == 97) { //number 1, or numpad 1 
-       ourPlayer.speedMultiplier=5;
-       ourPlayer.playerId.innerHTML="Speed 1";
+       ourPlayer1.speedMultiplier=5;
+       ourPlayer1.playerId.innerHTML="Speed 1";
+       ourPlayer2.speedMultiplier=5;
+    ourPlayer2.playerId.innerHTML="Speed 1";
    }
    if(e.keyCode == 50 || e.keyCode == 98) { //number 2, or numpad 2
-       ourPlayer.speedMultiplier=15;
-       ourPlayer.playerId.innerHTML="Speed 2";
+       ourPlayer1.speedMultiplier=15;
+       ourPlayer1.playerId.innerHTML="Speed 2";
+       ourPlayer2.speedMultiplier=15;
+        ourPlayer2.playerId.innerHTML="Speed 2";
    }
    if(e.keyCode == 51 || e.keyCode == 99) { //number 3, or numpad 3
-       ourPlayer.speedMultiplier=35;
-       ourPlayer.playerId.innerHTML="Speed 3";
+       ourPlayer1.speedMultiplier=35;
+       ourPlayer1.playerId.innerHTML="Speed 3";
+       ourPlayer2.speedMultiplier=35;
+        ourPlayer2.playerId.innerHTML="Speed 3";
    }
 
    let theKeyCode = e.keyCode || e.which; //Find out which key was pressed
@@ -54,14 +95,16 @@ function theControls(e) {
 }
 
 function scrollIt() { //Camera Follows the Player
-    ourPlayer.playerId.scrollIntoView({block: "center", inline: "center"});
+    ourPlayer1.playerId.scrollIntoView({block: "center", inline: "center"});
 }
 
 function doThisLoop() {
-    playerMotion();
+    playerMotion1();
+    playerMotion2();
     scrollIt();     //Camera Follows Player
     requestAnimationFrame(doThisLoop);  
 }
 
 doThisLoop();
-movePlayer(); //Update the Position of the player
+movePlayer1(); //Update the Position of the player
+movePlayer2(); //Update the Position of the player
