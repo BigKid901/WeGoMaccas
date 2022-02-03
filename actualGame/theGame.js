15663df62e4628b978732efc57e22ef498fac88b
Camron Braden <CamronRBraden@gmail.com>
2022-02-03 17:54:28 -0600

    new game template

--- actualGame/theGame.js
+++ actualGame/theGame.js
@@ -6,17 +6,27 @@ let keyboard = {}; //This is the link for keycodes to change controls http://gcc
     keyboard.LEFT = 65;   /*A*/     keyboard.LEFT_AGAIN = 74;   /*K*/
     keyboard.RIGHT = 68;  /*D*/     keyboard.RIGHT_AGAIN = 76;  /*L*/
     
-let ourPlayer1 = { x: 100, y: 100, speedMultiplier: 35, playerId: document.getElementById("thePlayer1") };
+let ourPlayer1 = { x: 100, y: 100, speedMultiplier: 35,playerId: document.getElementById("thePlayer1") };
 let movePlayer1 = function(theX, theY) { /* Character Movement Updating */
     ourPlayer1.x += (theX||0) * ourPlayer1.speedMultiplier; ourPlayer1.y += (theY||0) * ourPlayer1.speedMultiplier;
     ourPlayer1.playerId.style.left = ourPlayer1.x + 'px'; ourPlayer1.playerId.style.top = ourPlayer1.y + 'px'; };
+
+let jump1 = function(theX, theY) {
+    
+}
+
 let playerMotion1 = function() { /* Player Controls */
     if(keyboard[keyboard.LEFT])  { movePlayer1(-1,0); }
     if(keyboard[keyboard.RIGHT]) { movePlayer1(1,0);  }
-    if(keyboard[keyboard.UP])    { movePlayer1(0,-1); }
-    if(keyboard[keyboard.DOWN])  { movePlayer1(0,1);  } }
+    if(keyboard[keyboard.UP])    { 
+        movePlayer1(0,-1); 
+    }
+    if(keyboard[keyboard.DOWN])  { movePlayer1(0,1);  } 
 
-let ourPlayer2 = { x: 100, y: 100, speedMultiplier: 35,playerId: document.getElementById("thePlayer2") };
+
+}
+
+let ourPlayer2 = { x: 100, y: 100, speedMultiplier: 35, playerId: document.getElementById("thePlayer2") };
 let movePlayer2 = function(theX, theY) { /* Character Movement Updating */
     ourPlayer2.x += (theX||0) * ourPlayer2.speedMultiplier; ourPlayer2.y += (theY||0) * ourPlayer2.speedMultiplier;
     ourPlayer2.playerId.style.left = ourPlayer2.x + 'px'; ourPlayer2.playerId.style.top = ourPlayer2.y + 'px'; };
